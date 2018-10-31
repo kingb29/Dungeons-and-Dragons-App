31cfab994ddf270bad1ed7a1a9d0a0d45b5d6a40
kelsey-amyotte <lorenk@uwosh.edu>
2018-10-31 12:33:37 -0500

    init

---
+++ dungeons-and-dragons-app/dungeons-and-dragons-app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dungeons_and_dragons_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
