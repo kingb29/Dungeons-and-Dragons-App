587aa17141e875a1554246c5d4921b8a60d5ccab
kingb29 <kingb29@uwosh.edu>
2018-11-16 18:47:52 -0600

    Added Login button to homepage, Added Login Page, Added authentication of username/password with accounts already in db.

---
+++ dungeons-and-dragons-app/dungeons-and-dragons-app/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dungeons_and_dragons_app.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace dungeons_and_dragons_app.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly string connectionString;
+
+        public AccountController(AppSetting appSetting)
+        {
+            connectionString = appSetting.ConnectionString;
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string hashPass)
+        {
+            if (username != null && hashPass != null)
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+                        string query = "SELECT `HashPass`, `Username` FROM `User` WHERE username = @username and hashPass=@hashPass";
+                        MySqlCommand command = new MySqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@username", username);
+                        command.Parameters.AddWithValue("@hashPass", hashPass);
+                        var count = command.ExecuteScalar();
+
+                        if (count != null) // test if correct login details
+                        {
+                            // do stuff to make the user "signed in"
+                            return View("~/Views/Home/Index.cshtml");
+                        }
+
+                        connection.Close();
+                    }
+                    catch (MySqlException e)
+                    {
+                        Console.Write(e);
+                    }
+                    
+                }
+            }
+                ViewBag.error = "Invalid Account";
+                return View("~/Views/Home/Login.cshtml");
+        }
+
+
+
+        [Route("logout")]
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ dungeons-and-dragons-app/dungeons-and-dragons-app/ViewModels/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class LoginViewModel
+{
+    public string username { get; set; }
+    public int databaseID { get; set; }
+    public int[] characterSheetIDs { get; set; }
+    public string hashPass { get; set; }
+    public int salt { get; set; }
+
+    public LoginViewModel()
+    {
+
+    }
+
+    public LoginViewModel(int databaseID, int salt, string hashPass, string username, int[] characterSheetIDs)
+    {
+        this.databaseID = databaseID;
+        this.salt = salt;
+        this.hashPass = hashPass;
+        this.username = username;
+        this.characterSheetIDs = characterSheetIDs;
+    }
+
+}
+
