7dff81ff792cab921970d0a0e995b1077fef2c63
Kelsey Amyotte <Kelsey Lorenz>
2018-11-27 13:37:44 -0600

    spells and weapons in form now work. Also added some form validation.

--- dungeons-and-dragons-app/dungeons-and-dragons-app/ViewModels/CharacterViewModel.cs
+++ dungeons-and-dragons-app/dungeons-and-dragons-app/ViewModels/CharacterViewModel.cs
@@ -1,25 +1,59 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 public class CharacterViewModel
 {
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string name { get; set; }
+
     public string gender { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[1-9]\d*$")]
     public string level { get; set; }
+
     public string alignment { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[1-9]\d*$")]
     public string str { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[1-9]\d*$")]
     public string dex { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[1-9]\d*$")]
     public string con { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[1-9]\d*$")]
     public string inte { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[1-9]\d*$")]
     public string wis { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[1-9]\d*$")]
     public string cha { get; set; }
+
     public string race { get; set; }
+
     public string charClass { get; set; }
+
     public string weapon { get; set; }
+
     public string spell { get; set; }
+
+    public int[] spells { get; set; }
+
+    public int[] weapons { get; set; }
 
     public IEnumerable<SelectListItem> raceList { get; set; }
     public IEnumerable<SelectListItem> classList { get; set; }
